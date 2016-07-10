4fc75d5e9c89c124ae8dc3ac0bf80dd6cd845fed
sperku <sperku@gmail.com>
2016-07-10 22:20:59 +0900

    Update cipher.js

--- es6/src/cipher.js
+++ es6/src/cipher.js
@@ -7,30 +7,12 @@ export class Cipher {
 
 export class CaesarCipher extends Cipher {
   constructor(int){
-    super(str);
-    super.encrypt(str);
     this.int = int;
-     var arr = str.split("");
-    
-    var alf = "abcdefghijklmnopqrstuvwxyz";
-    
-    function Encrypter (element, index, array){
-    var place = alf.indexOf(element); 
-    place += this.int;
-    var newChar = alf.charAt(place);
-    array[index] = newChar;
-    }
-    
-    arr.forEach(Encrypter);
-    var str2 = arr.join('');
-    return str2;
-    
   }
   
   encrypt(str){
     super(str);
     super.encrypt(str);
-    this.int = int;
      var arr = str.split("");
     
     var alf = "abcdefghijklmnopqrstuvwxyz";
